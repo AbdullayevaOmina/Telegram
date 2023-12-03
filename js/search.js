c8e369ccca1fc8bdd7cdd3c83f5a06d7f8973bf8
Omina <ominaabdullayeva76@gmail.com>
2023-12-03 20:14:31 +0500

    first commit

---
+++ js/search.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", async function () {
+  axios.defaults.baseURL = "http://localhost:3000";
+  let serch_input = document.querySelector("#serch-input");
+  let search_btn = document.querySelector(".search_btn");
+	
+});
